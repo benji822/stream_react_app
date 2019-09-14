f16f6caafb55fd796a7f73fafa34218b1d3c1653
benjamin <benng822@gmail.com>
2019-09-14 11:08:47 +0800

    done with simple CRUD functionality with react app

---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,44 @@
+import _ from "lodash";
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
+
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams(this.props.match.params.id);
+    }
+
+    onSubmit = formValues => {
+        this.props.editStream(this.props.match.params.id, formValues);
+    };
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div className="">
+                <h3>Edit a Stream</h3>
+                <StreamForm
+                    onSubmit={this.onSubmit}
+                    initialValues={_.pick(
+                        this.props.stream,
+                        "title",
+                        "description"
+                    )}
+                />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(
+    mapStateToProps,
+    { fetchStreams, editStream }
+)(StreamEdit);
